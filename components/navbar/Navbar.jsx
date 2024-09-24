06b0c64bec2230019868f9c95adc6dbe8983d87a
Eyader Tsehayu <eyaderts@gmail.com>
2024-09-24 13:00:56 +0300

    add client nav bar

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,91 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "/public/images/logo.png";
+
+//import "../styles/annimations.css";
+
+const Menu = () => (
+  <>
+    {" "}
+    <p className=" hover:text-primary">
+      <a href="#Breakfast">Home</a>
+    </p>
+    <p className=" hover:text-primary">
+      <a href="#Lunch">Orders</a>
+    </p>
+    <p className=" hover:text-primary">
+      <a href="#Dinner">Who we are</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <nav className="w-full z-10">
+      <div className="flex gap-5 items-center px-2 md:pt-6  sm:mb-4 mb-3 sm:pt-4 pt-2 text-xl justify-between">
+        <div className="flex justify-start">
+          <Image
+            src={logo}
+            alt="logo"
+            objectFit="cover"
+            width={130}
+            height={50}
+            className="w-full z-10 h-full top-0 right-0 object-cover "
+          />
+        </div>
+        <div className="hidden md:flex flex-1 justify-center">
+          <div className="flex flex-row gap-24">
+            <Menu />
+          </div>
+        </div>
+        <div className=" sm:block hidden justify-end">
+          <button className="bg-primary hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md">
+            Register
+          </button>
+        </div>
+
+        {/* MOBILE NAVIGATION */}
+        <div className="ml-1 pr-4 relative sm:hidden">
+          {toggleMenu ? (
+            <RiCloseLine
+              className="text-primary"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              className="text-primary"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="flex  z-20 flex-col  absolute m-1 drop-shadow-lg items-center">
+              <div className="absolute text-base flex-col gap-8 w-56 bg-white items-start text-left pl-4 pr-16 pb-4 rounded-lg">
+                <Menu />
+                <button className="bg-primary text-white font-semibold my-2 py-1 px-2 rounded-md">
+                  Register
+                </button>
+              </div>{" "}
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
