9a59a233f9ab74e3190c8174c8cec16f77e2f1f8
Eyader Tsehayu <eyaderts@gmail.com>
2024-09-25 19:44:24 +0300

    start some basic admin dashboard pages

---
+++ app/(admin)/dashboard/layout.jsx
@@ -0,0 +1,25 @@
+"use client";
+import Navbar from "@/components/dashboard/navbar/navbar";
+import Sidebar from "@/components/dashboard/sidebar/sidebar";
+import { useState } from "react";
+import "./data-tables-css.css";
+const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {" "}
+          <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <main>
+            <div className="mx-auto max-w-screen-2xl   ">{children}</div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app/(admin)/dashboard/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Dashboard = () => {
+  return <div className="bg-[#f8f8f8] h-screen w-full">admin main page</div>;
+};
+
+export default Dashboard;
