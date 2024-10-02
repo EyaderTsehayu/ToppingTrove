26ed0c4c2f232ca8955181b0dc413b528f07e88c
Eyader Tsehayu <eyaderts@gmail.com>
2024-10-02 18:52:22 +0300

    add restaurant registration page

---
+++ app/(auth)/register/add-admin/page.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import logo from "/public/images/logo.png";
+import SideUi from "@/components/ui/SideUi";
+import SignInForm from "@/components/forms/SigninForm";
+import AddAdminForm from "@/components/forms/AddAdminForm";
+
+const page = () => {
+  return (
+    <div className="flex h-screen">
+      <SideUi />
+
+      <section className="remove-scrollbar container my-auto w-full md:w-1/2">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={logo}
+            height={50}
+            width={130}
+            alt="logo"
+            className="mb-8"
+          />
+          <h1 className="mb-2 text-xl font-normal">Add Admin</h1>
+          <hr className="mb-6" />
+          <AddAdminForm />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(auth)/register/page.jsx
@@ -0,0 +1,29 @@
+import RegistrationForm from "@/components/forms/RegistrationForm";
+import Image from "next/image";
+import logo from "/public/images/logo.png";
+import SideUi from "@/components/ui/SideUi";
+import RestaurantRegistrationForm from "@/components/forms/RestaurantRegistrationForm";
+
+const page = () => {
+  return (
+    <div className="flex h-screen">
+      <SideUi />
+
+      <section className="remove-scrollbar container my-auto w-full md:w-1/2">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={logo}
+            height={50}
+            width={130}
+            alt="logo"
+            className="mb-16"
+          />
+
+          <RestaurantRegistrationForm />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
