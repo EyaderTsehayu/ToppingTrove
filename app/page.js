899aaf01e522bc9955d4522c14fb454d13d04d34
Eyader Tsehayu <eyaderts@gmail.com>
2024-10-04 22:44:59 +0300

    call footer component

--- app/page.js
+++ app/page.js
@@ -5,8 +5,19 @@ import TopRestaurant from "@/components/Feature/TopRestaurant";
 import Footer from "@/components/footer/Footer";
 import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    const resId = session?.user?.restaurantId;
+    // console.log("REstaurant IDDddddd", resId);
+    if (resId != null) {
+      redirect("/dashboard/orders");
+    }
+  }
   return (
     <div>
       <Navbar />
@@ -18,7 +29,7 @@ export default function Home() {
       <TopRestaurant />
       <PopularPizza />
       <Fasting />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
